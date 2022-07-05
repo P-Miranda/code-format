1399b5b225bbd26b177709aadf043a847913e1a9
P-Miranda <P-Miranda@users.noreply.github.com>
2022-07-05 17:25:57 +0000

    Apply formatting changes

--- src/c/main.c
+++ src/c/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[]){
-    printf("This is a test program");
-    for (int i=0; i<argc; i++){ printf("%s\n", argv[i]); }
+int main(int argc, char *argv[]) {
+  printf("This is a test program");
+  for (int i = 0; i < argc; i++) {
+    printf("%s\n", argv[i]);
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
